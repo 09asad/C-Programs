c4863a23bf96a4a9e9e5639c99ec9a088c492188
09asad <148388760+09asad@users.noreply.github.com>
2023-12-26 15:09:40 +0530

    Update oddNumberTriangle.c

--- C Program/pattern/oddNumberTriangle.c
+++ C Program/pattern/oddNumberTriangle.c
@@ -7,7 +7,7 @@ int main()
     for(int i=1;i<=n;i++)       // no. of lines/rows -> i
     {
         int a=1;
-        for(int j=1;j<=n;j=j++)     // no. of columns -> j
+        for(int j=1;j<=i;j=j++)     // no. of columns -> j
         {
             printf("%d ",a);  
             a=a+2;     
